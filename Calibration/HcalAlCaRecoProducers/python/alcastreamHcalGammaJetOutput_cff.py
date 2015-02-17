341756d61d4765625da6ad08eadaab5e4998a1c2
Andrius Juodagalvis <andjuo@hotmail.com>
2015-02-17 18:48:32 +0100

    alcastreamHcalGammaJetOutput_cff.py changed outputCommands to match ALCARECOHcalCalGammaJet_Output_cff.py

--- Calibration/HcalAlCaRecoProducers/python/alcastreamHcalGammaJetOutput_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/alcastreamHcalGammaJetOutput_cff.py
@@ -4,7 +4,12 @@ import FWCore.ParameterSet.Config as cms
 # output module 
 #  module alcastreamHcalGammaJetOutput = PoolOutputModule
 alcastreamHcalGammaJetOutput = cms.PSet(
-    outputCommands = cms.untracked.vstring('drop *', 
-        'keep *_GammaJetProd_*_*')
+    outputCommands = cms.untracked.vstring('drop *',
+                 'keep recoPhotonCores_*_*_*',
+                 'keep recoSuperClusters_*_*_*',
+                 'keep recoTracks_generalTracks_*_*',
+                 'keep *_particleFlow_*_*',
+                 'keep recoPFBlocks_particleFlowBlock_*_*',
+                 'keep recoPFClusters_*_*_*',
+                 'keep *_GammaJetProd_*_*')
 )
-

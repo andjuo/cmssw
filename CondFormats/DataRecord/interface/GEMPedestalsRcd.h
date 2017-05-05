9fe9e7cfc3f6dfc6e2b3f96cb18054dd28ff155d
Aivaras <aivaras.silale@gmail.com>
2017-05-05 12:44:53 +0200

    added GEMPedestalsRcd records

---
+++ CondFormats/DataRecord/interface/GEMPedestalsRcd.h
@@ -0,0 +1,25 @@
+#ifndef GEMPedestalsRcd_GEMPedestalsRcd_h
+#define GEMPedestalsRcd_GEMPedestalsRcd_h
+// -*- C++ -*-
+//
+// Package:     CondFormats/DataRecord
+// Class  :     GEMPedestalsRcd
+// 
+/**\class GEMPedestalsRcd GEMPedestalsRcd.h CondFormats/DataRecord/interface/GEMPedestalsRcd.h
+
+ Description: [one line class summary]
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      Aivaras Silale
+// Created:     Fri, 05 May 2017 10:33:13 GMT
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class GEMPedestalsRcd : public edm::eventsetup::EventSetupRecordImplementation<GEMPedestalsRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/src/GEMPedestalsRcd.cc
@@ -0,0 +1,15 @@
+// -*- C++ -*-
+//
+// Package:     CondFormats/DataRecord
+// Class  :     GEMPedestalsRcd
+// 
+// Implementation:
+//     [Notes on implementation]
+//
+// Author:      Aivaras Silale
+// Created:     Fri, 05 May 2017 10:33:13 GMT
+
+#include "CondFormats/DataRecord/interface/GEMPedestalsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(GEMPedestalsRcd);

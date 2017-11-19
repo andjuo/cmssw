46a1a9696dea9caeaeb66c2b9cfffd5c09601a52
Andrius Juodagalvis <andjuo@hotmail.com>
2017-11-19 12:02:20 +0100

    GEMEMapSourceHandler.cc operates in listing mode

--- CondTools/GEM/src/GEMEMapSourceHandler.cc
+++ CondTools/GEM/src/GEMEMapSourceHandler.cc
@@ -8,6 +8,11 @@
 #include "RelationalAccess/ITransaction.h"
 #include "RelationalAccess/ISchema.h"
 #include "RelationalAccess/ITable.h"
+#include "RelationalAccess/ITableDescription.h"
+#include "RelationalAccess/IColumn.h"
+#include "RelationalAccess/IPrimaryKey.h"
+#include "RelationalAccess/IIndex.h"
+#include "RelationalAccess/IView.h"
 #include "RelationalAccess/IQuery.h"
 #include "RelationalAccess/ICursor.h"
 #include "CoralBase/AttributeList.h"
@@ -22,6 +27,10 @@
 #include <sstream>
 
 #include <DataFormats/MuonDetId/interface/GEMDetId.h>
+
+inline void HERE(std::string msg)
+{ std::cout << msg << std::endl; }
+
 
 popcon::GEMEMapSourceHandler::GEMEMapSourceHandler( const edm::ParameterSet& ps ):
   m_name( ps.getUntrackedParameter<std::string>( "name", "GEMEMapSourceHandler" ) ),
@@ -197,8 +206,8 @@ void popcon::GEMEMapSourceHandler::ConnectOnlineDB( const std::string& connect, 
   connection.setParameters( connectionPset );
   std::cout << "connectionPset = " << connectionPset << "\n";
   connection.configure();
-  //session = connection.createSession( connect,true );
-  session = connection.createSession( connect,false ); // not writeCapable
+  session = connection.createSession( connect,true );
+  //session = connection.createSession( connect,false ); // not writeCapable
   edm::LogInfo( "GEMEMapSourceHandler" ) << "[" << "GEMEMapSourceHandler::" << __func__ << "]:" << m_name << ": "
                                          << "Done." << std::endl;
   std::cout << "GEMEMapSourceHandler ConnectOnlineDB leaving" << std::endl;
@@ -225,23 +234,106 @@ void popcon::GEMEMapSourceHandler::readGEMEMap()
     std::cout << "\n\ttables.size=" << tables.size() << ", views.size=" << views.size() << "\n" << std::endl;
 
     if (tables.size()) {
-      std::cout << "tables:\n";
+      std::cout << "\ntables:\n";
       int idx=0;
       for (std::set<std::string>::const_iterator it= tables.begin();
 	   it!=tables.end(); it++, idx++) {
-	std::cout << "table idx=" << idx << ": " << *it << "\n";
+	std::cout << "\ntable idx=" << idx << ": " << *it << "\n";
+	//HERE("get table handle");
+	const coral::ITable* tableHandlePtr= & schema.tableHandle(*it);
+	if (!tableHandlePtr) {
+	  std::cout << "failed to get tableHandlePtr\n";
+	  return;
+	}
+	//HERE("get table description");
+	const coral::ITableDescription* d = & tableHandlePtr->description();
+	if (!d) {
+	  std::cout << "failed to get tableDescriptionPtr\n";
+	  return;
+	}
+	//const coral::ITableDescription* d= tableDescrPtr;
+	//HERE("list contents");
+	try {
+	  std::cout << "name=" << d->name() << ", type=" << d->type() << ", numberOfColumns=" << d->numberOfColumns() << ", hasPrimaryKey=" << d->hasPrimaryKey() << ", numberOfIndices=" << d->numberOfIndices() << ", numberOfForeignKeys=" << d->numberOfForeignKeys() << ", numberOfUniqueConstraints=" << d->numberOfUniqueConstraints() << "\n";
+	  if (d->hasPrimaryKey()) {
+	    const coral::IPrimaryKey *pkey= & d->primaryKey();
+	    if (!pkey) {
+	      std::cout << "failed to get primary key\n";
+	      return;
+	    }
+	    const std::vector<std::string> & colNames= pkey->columnNames();
+	    std::cout << " ** PrimaryKey" << " tableSpaceName=" << pkey->tableSpaceName() << " has " << colNames.size() << " columnNames: ";
+	    for (unsigned int icol=0; icol<colNames.size(); icol++) {
+		std::cout << " " << colNames[icol];
+	    }
+	    std::cout << "\n";
+	  }
+	  if (d->numberOfIndices()) {
+	    if (d->numberOfIndices()>1) std::cout << " ** Several indices! (listing)\n";
+	    for (int ii=0; ii<d->numberOfIndices(); ii++) {
+	      const  coral::IIndex *iidx= & d->index(ii);
+	      if (!iidx) {
+		std::cout << "failed to get index at ii=" << ii << "\n";
+		return;
+	      }
+	      std::cout << " ** Index @" << ii << ". name=" << iidx->name() << ", isUnique=" << iidx->isUnique() << ", tableSpaceName=" << iidx->tableSpaceName() << "\n";
+	      const std::vector<std::string> & colNames=iidx->columnNames();
+	      std::cout << "  has " << colNames.size() << " columns: ";
+	      for (unsigned int icol=0; icol<colNames.size(); icol++) {
+		std::cout << " " << colNames[icol];
+	      }
+	      std::cout << "\n";
+	    }
+	  }
+	  if (d->numberOfColumns()) {
+	    //HERE("list columns");
+	    std::cout << " -- Columns: \n";
+	    for (int iCol=0; iCol<d->numberOfColumns(); iCol++) {
+	      const coral::IColumn* col= & d->columnDescription(iCol);
+	      std::cout << "iCol=" << iCol << ", name=" << col->name() << ", C++ type=" << col->type() << ", indexInTable=" << col->indexInTable() << ", isNotNull=" << col->isNotNull() << ", isUnique=" << col->isUnique() << ", size=" << col->size() << ", isSizeFixed=" << col->isSizeFixed() << "\n";
+	    }
+	  }
+	}
+	catch ( const std::exception & e ) {
+	  std::cout << "exception " << e.what() << " caught\n";
+	  continue;
+	}
       }
       std::cout << std::endl;
     }
     if (views.size()) {
-      std::cout << "views:\n";
+      std::cout << "\nviews:\n";
       int idx=0;
       for (std::set<std::string>::const_iterator it= views.begin();
 	   it!=views.end(); it++, idx++) {
-	std::cout << "views idx=" << idx << ": " << *it << "\n";
+	std::cout << "\nviews idx=" << idx << ": " << *it << "\n";
+	try {
+	const coral::IView *viewH= & schema.viewHandle(*it);
+	if (!viewH) {
+	  std::cout << "failed to get view\n";
+	  return;
+	}
+	std::cout << " ** has " << viewH->numberOfColumns() << " columns\n";
+	    if (idx!=4)
+	if (viewH->numberOfColumns()) {
+	  //HERE("list view columns");
+	  std::cout << " -- Columns: \n";
+	  for (int iCol=0; iCol<viewH->numberOfColumns(); iCol++) {
+	    const coral::IColumn* col= & viewH->column(iCol);
+	    std::cout << "iCol=" << iCol << ", name=" << col->name();
+	    std::cout << " C++ type=" << col->type();
+	    std::cout << ", indexInTable=" << col->indexInTable() << ", isNotNull=" << col->isNotNull() << ", isUnique=" << col->isUnique() << ", size=" << col->size() << ", isSizeFixed=" << col->isSizeFixed() << "\n";
+	  }
+	}
+	}
+	catch ( const std::exception &e ) {
+	  std::cout << "exception " << e.what() << " caught\n";
+	}
       }
       std::cout << std::endl;
     }
+    std::cout << "code operates to list tables and views. exciting..." << std::endl;
+    return;
   }
 
   std::string condition="";
